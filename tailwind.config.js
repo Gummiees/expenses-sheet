305b77d437e0c940454cb4c5f4d9a76ceb087d11
Gummiees <xavier.caro@koahealth.com>
2022-10-24 21:16:14 +0200

    still initialising the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
