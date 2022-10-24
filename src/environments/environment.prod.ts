b4cb49c79708b96bc8294b3fdafce8236d5304c3
Gummiees <xavier.caro@koahealth.com>
2022-10-24 21:01:27 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'expenses-sheet-914e3',
+    appId: '1:682048451719:web:25d786650633244412b422',
+    storageBucket: 'expenses-sheet-914e3.appspot.com',
+    apiKey: 'AIzaSyAdIJ6rRlSEGomPgnUV3pM74AauLVbJxDI',
+    authDomain: 'expenses-sheet-914e3.firebaseapp.com',
+    messagingSenderId: '682048451719',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'expenses-sheet-914e3',
+    appId: '1:682048451719:web:25d786650633244412b422',
+    storageBucket: 'expenses-sheet-914e3.appspot.com',
+    apiKey: 'AIzaSyAdIJ6rRlSEGomPgnUV3pM74AauLVbJxDI',
+    authDomain: 'expenses-sheet-914e3.firebaseapp.com',
+    messagingSenderId: '682048451719',
+  },
   production: false
 };
 
